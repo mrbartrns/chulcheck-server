89dd8e61c03e3c3379845d5c68050cc0ef6d4609
Junhyuck Ko <mrbartrns@naver.com>
2022-11-27 23:39:40 +0900

    feat: api 추가

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import AttendanceView
+from .views import AttendanceView, OrganizationView
 
 
 urlpatterns = [
     path("attendance", AttendanceView.as_view()),
     path("attendance/<int:month>", AttendanceView.as_view()),
+    path("organizations/<int:id>", OrganizationView.as_view()),
 ]
