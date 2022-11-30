1c71727a9f7b923cf4c95165701664df7ab206a7
Junhyuck Ko <mrbartrns@naver.com>
2022-11-30 13:49:38 +0900

    feat: 그룹 조인 기능 추가

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import AttendanceView, OrganizationView, OrganizationListView
+from .views import (
+    AttendanceView,
+    OrganizationView,
+    OrganizationListView,
+    OrganizationUserView,
+)
 
 
 urlpatterns = [
     path("attendance/<int:month>", AttendanceView.as_view()),
     path("organizations", OrganizationListView.as_view()),
     path("organizations/<int:id>", OrganizationView.as_view()),
+    path("organizations/<int:id>/users", OrganizationUserView.as_view()),
 ]
