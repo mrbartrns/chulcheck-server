ec38c3fb424077e0fd702024656fa03ffa99841a
Junhyuck Ko <mrbartrns@naver.com>
2022-12-08 14:46:17 +0900

    feat: attendance list view 변경

--- api/views.py
+++ api/views.py
@@ -24,6 +24,18 @@ class AttendanceListView(APIView):
     def get(self, request):
         user = request.user
         data = Attendance.objects.filter(user=user)
+
+        # djagno queryset are executed lazily
+        if request.query_params:
+            month = request.query_params.get("month", None)
+            organization = request.query_params.get("organization", None)
+
+            if month:
+                data = data.filter(timestamp__month=month)
+
+            if organization:
+                data = data.filter(organization__id=organization)
+
         return Response(
             self.serializer_class(data, many=True).data, status=status.HTTP_200_OK
         )
@@ -31,8 +43,11 @@ class AttendanceListView(APIView):
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
 
+        organization_id = request.data.get("organization", None)
+        organization = get_object_or_404(Organization, id=organization_id)
+
         if serializer.is_valid():
-            data = serializer.save(user=request.user)
+            data = serializer.save(user=request.user, organization=organization)
             return Response(
                 self.serializer_class(data).data, status=status.HTTP_201_CREATED
             )
@@ -43,8 +58,7 @@ class AttendanceListView(APIView):
 class AttendanceView(APIView):
     """
     출석 정보의 get(list)과 post를 담당하는 view 입니다.
-    TODO: post, update, delete method 추가
-    ANCHOR: AllowAny -> authenticated로 변경 필요
+    TODO - 출석 시 출석 아이디로 정보를 얻어오는 view로 바꾸기
     """
 
     permission_classes = [IsAuthenticated]
