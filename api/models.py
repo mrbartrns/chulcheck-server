bb4b574a938b17eaca455156f8bd2213559ecbf5
Junhyuck Ko <mrbartrns@naver.com>
2022-12-08 14:18:04 +0900

    feat: Attendance에 Organization 추가

--- api/models.py
+++ api/models.py
@@ -4,22 +4,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-class Attendance(models.Model):
-    user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        verbose_name="user",
-        related_name="attendance_data",
-    )
-    timestamp = models.DateTimeField(verbose_name="timestamp", auto_now_add=True)
-
-    class Meta:
-        ordering = ["timestamp"]
-
-    def __str__(self):
-        return f"{self.user} at {self.timestamp}"
-
-
 class Organization(models.Model):
     name = models.CharField(verbose_name="name", max_length=25)
     leader = models.ForeignKey(
@@ -31,3 +15,22 @@ class Organization(models.Model):
 
     def __str__(self) -> str:
         return f"{self.name}"
+
+
+class Attendance(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="user",
+        related_name="attendance_data",
+    )
+    organization = models.ForeignKey(
+        Organization, on_delete=models.SET_NULL, null=True, related_name="attendances"
+    )
+    timestamp = models.DateTimeField(verbose_name="timestamp", auto_now_add=True)
+
+    class Meta:
+        ordering = ["timestamp"]
+
+    def __str__(self):
+        return f"{self.user} at {self.timestamp}"
